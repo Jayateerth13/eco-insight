481f6b8252e94302bd8be68d4162cbe712ddf7b1
Jayateerth Kamatgi <jayupk@gmail.com>
2025-10-18 16:05:25 -0700

    Eco Insight App code

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Home from "./pages/Home";
+import SustainabilityAssistant from "./components/SustainabilityAssistant";
+
+function App() {
+  return <SustainabilityAssistant />;
+}
+
+export default App;
